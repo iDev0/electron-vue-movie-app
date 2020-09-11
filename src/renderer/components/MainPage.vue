62e4fed4481fea28d62bc27556e7f57a53659066
iDev0 <insert@kakao.com>
2020-09-11 16:57:31 +0900

    key 추가

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -1,7 +1,8 @@
 <template>
     <div id="wrapper">
         <h1>HELLO MOVIE APP!!</h1>
-        <ul v-for="movie in nowPlaying" :bind="movie.id">
+        <ul v-for="movie in nowPlaying" :key="movie.id">
+          <li>영화제목 : {{ movie.title }}</li>
           <li>영화제목 : {{ movie.title }}</li>
           <img v-bind:src="poster(movie.poster_path)" width="300" height="300"/>
         </ul>
