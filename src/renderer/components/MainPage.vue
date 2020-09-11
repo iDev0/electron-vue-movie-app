f476bc5c7c0c8d68c0c9537f79bd69064b3940a2
iDev0 <insert@kakao.com>
2020-09-11 16:55:44 +0900

    타이틀 및 이미지 포스터 불러오기, eslint 설정

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -1,15 +1,35 @@
 <template>
     <div id="wrapper">
         <h1>HELLO MOVIE APP!!</h1>
+        <ul v-for="movie in nowPlaying" :bind="movie.id">
+          <li>영화제목 : {{ movie.title }}</li>
+          <img v-bind:src="poster(movie.poster_path)" width="300" height="300"/>
+        </ul>
     </div>
 </template>
-<script>
-export default {
-    name : 'main-page',
 
+<script>
+import { movieAPI, apiImage } from '../api'
+
+export default {
+  data : function () {
+    return { nowPlaying : [] }  
+  },
+  methods : {
+    poster : function(path) {
+      return apiImage(path)
+    }
+  }, 
+  mounted () {
+    movieAPI.nowPlaying()
+      .then(([result, error]) => {
+        console.log(result);
+        this.nowPlaying = result
+      });
+  }
 }
 </script>
 
-<style scoped>
+<style>
 
 </style>
