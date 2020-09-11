736df221ec398e1465c25532e74d913a5a3fda98
idev <insert@kakao.com>
2020-09-12 01:13:28 +0900

    주석 처리

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -1,9 +1,11 @@
 <template>
     <div id="wrapper">
-        <h1>HELLO MOVIE APP!!</h1>
+        <h1>{{ title }}</h1>
+        <!-- 4번 영화데이터 for문 돌리기 -->
         <ul v-for="movie in nowPlaying" :key="movie.id">
+            <!-- json data 를 불러옴 -->
           <li>영화제목 : {{ movie.title }}, 영화평점 : {{ movie.vote_average }}</li><br/>
-          <img :src="poster(movie.poster_path)" width="300" height="300"/>
+          <img :src="poster(movie.poster_path)" width="400" height="400"/>
         </ul>
     </div>
 </template>
@@ -12,8 +14,12 @@
 import { movieAPI, apiImage } from '../api'
 
 export default {
+
   data : function () {
-    return { nowPlaying : [] }  
+    // 1번 변수 생성
+    return {
+      nowPlaying : []
+    }
   },
   methods : {
     poster : function(path) {
@@ -21,9 +27,11 @@ export default {
     }
   }, 
   mounted () {
+    // 2번 영화 데이터 호출
     movieAPI.nowPlaying()
       .then(([result, error]) => {
         console.log(result);
+        // 3번 생성한 1번 변수에 영화 데이터 저장
         this.nowPlaying = result
       });
   }
