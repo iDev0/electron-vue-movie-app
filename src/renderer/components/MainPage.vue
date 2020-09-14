c7d554832e8a3df462f3df75e04038451f59bd61
iDev0 <insert@kakao.com>
2020-09-14 13:12:14 +0900

    spinner 추가

--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -1,13 +1,22 @@
 <template>
     <div id="wrapper">
-        <h1>
-            {{ title.length > 0 ? title : '타이틀을 찾을 수 없습니다. :(' }}
-        </h1>
-        <!-- 4번 영화데이터 for문 돌리기 -->
-        <ul v-for="movie in nowPlaying" :key="movie.id">
-            <!-- json data 를 불러옴 -->
-          <li>영화제목 : {{ movie.title }}, 영화평점 : {{ movie.vote_average }}</li><br/>
-            <Poster :path="movie.poster_path" width="300" height="300" />
+        <!-- loading spinner -->
+        <div class="loadingio-spinner-spinner-x7ejdclnggh" v-show="loading">
+            <div class="ldio-kqajj5wfe5">
+                <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+            </div>
+        </div>
+
+        <ul v-for="movie in nowPlaying" :key="movie.id" v-show="!loading">
+            <transition name="fade">
+                <li style="list-style: none">
+                    <div>
+                        <Poster :path="movie.poster_path" width="300" height="300" />
+                        <p> 영화제목 : {{ movie.title }}</p>
+                        <p>영화평점 : {{ movie.vote_average }} </p>
+                    </div>
+                </li>
+            </transition>
         </ul>
     </div>
 </template>
@@ -27,7 +36,8 @@ export default {
   data : function () {
     // 1번 변수 생성
     return {
-      nowPlaying : []
+        loading : true,
+        nowPlaying : []
     }
   },
   methods : {
@@ -41,7 +51,8 @@ export default {
       .then(([result, error]) => {
         console.log(result);
         // 3번 생성한 1번 변수에 영화 데이터 저장
-        this.nowPlaying = result
+          this.nowPlaying = result
+          this.loading = false
       });
   }
 }
@@ -51,4 +62,97 @@ export default {
 ul {
     float: left;
 }
+ul li {
+    height: 400px;
+    border: 1px solid;
+    padding: 10px;
+    border-radius: 15px;
+}
+
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+}
+
+@keyframes ldio-kqajj5wfe5 {
+    0% { opacity: 1 }
+    100% { opacity: 0 }
+}
+.ldio-kqajj5wfe5 div {
+    left: 94px;
+    top: 48px;
+    position: absolute;
+    animation: ldio-kqajj5wfe5 linear 1s infinite;
+    background: #fe718d;
+    width: 12px;
+    height: 24px;
+    border-radius: 6px / 12px;
+    transform-origin: 6px 52px;
+}.ldio-kqajj5wfe5 div:nth-child(1) {
+     transform: rotate(0deg);
+     animation-delay: -0.9166666666666666s;
+     background: #fe718d;
+ }.ldio-kqajj5wfe5 div:nth-child(2) {
+      transform: rotate(30deg);
+      animation-delay: -0.8333333333333334s;
+      background: #fe718d;
+  }.ldio-kqajj5wfe5 div:nth-child(3) {
+       transform: rotate(60deg);
+       animation-delay: -0.75s;
+       background: #fe718d;
+   }.ldio-kqajj5wfe5 div:nth-child(4) {
+        transform: rotate(90deg);
+        animation-delay: -0.6666666666666666s;
+        background: #fe718d;
+    }.ldio-kqajj5wfe5 div:nth-child(5) {
+         transform: rotate(120deg);
+         animation-delay: -0.5833333333333334s;
+         background: #fe718d;
+     }.ldio-kqajj5wfe5 div:nth-child(6) {
+          transform: rotate(150deg);
+          animation-delay: -0.5s;
+          background: #fe718d;
+      }.ldio-kqajj5wfe5 div:nth-child(7) {
+           transform: rotate(180deg);
+           animation-delay: -0.4166666666666667s;
+           background: #fe718d;
+       }.ldio-kqajj5wfe5 div:nth-child(8) {
+            transform: rotate(210deg);
+            animation-delay: -0.3333333333333333s;
+            background: #fe718d;
+        }.ldio-kqajj5wfe5 div:nth-child(9) {
+             transform: rotate(240deg);
+             animation-delay: -0.25s;
+             background: #fe718d;
+         }.ldio-kqajj5wfe5 div:nth-child(10) {
+              transform: rotate(270deg);
+              animation-delay: -0.16666666666666666s;
+              background: #fe718d;
+          }.ldio-kqajj5wfe5 div:nth-child(11) {
+               transform: rotate(300deg);
+               animation-delay: -0.08333333333333333s;
+               background: #fe718d;
+           }.ldio-kqajj5wfe5 div:nth-child(12) {
+                transform: rotate(330deg);
+                animation-delay: 0s;
+                background: #fe718d;
+            }
+.loadingio-spinner-spinner-x7ejdclnggh {
+    width: 200px;
+    height: 200px;
+    display: inline-block;
+    overflow: hidden;
+    background: #ffffff;
+}
+.ldio-kqajj5wfe5 {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    transform: translateZ(0) scale(1);
+    backface-visibility: hidden;
+    transform-origin: 0 0; /* see note above */
+}
+.ldio-kqajj5wfe5 div { box-sizing: content-box; }
 </style>
